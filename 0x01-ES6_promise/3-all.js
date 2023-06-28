e3dc04633112931952d0eb9cca5ab6e1c3947888
Agidza <lynnagidza@gmail.com>
2023-06-28 23:41:28 +0300

    task 3 correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`body: ${photoResponse.body}`);
-      console.log(`firstName: ${userResponse.firstName}`);
-      console.log(`lastName: ${userResponse.lastName}`);
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
-
-export default handleProfileSignup;

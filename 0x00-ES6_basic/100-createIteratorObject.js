65c731bce7168b626b20e28935b29598758a7a32
Agidza <lynnagidza@gmail.com>
2023-06-28 17:08:47 +0300

    Eslint fixes

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,35 +1,33 @@
 export default function createIteratorObject(report) {
-    const departments = Object.values(report.allEmployees);
+  const departments = Object.values(report.allEmployees);
 
-    let currentDepartmentIndex = 0;
-    let currentEmployeeIndex = 0;
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
 
-    return {
-        next() {
-            const departmentEmployees = departments[currentDepartmentIndex];
-            const employee = departmentEmployees[currentEmployeeIndex];
-            if (employee) {
-                currentEmployeeIndex++;
-                return {
-                    value: employee,
-                    done: false,
-                };
-            } else {
-                currentDepartmentIndex++;
-                currentEmployeeIndex = 0;
-                const nextDepartmentEmployees = departments[currentDepartmentIndex];
+  return {
+    next() {
+      const departmentEmployees = departments[currentDepartmentIndex];
+      const employee = departmentEmployees[currentEmployeeIndex];
+      if (employee) {
+        currentEmployeeIndex++;
+        return {
+          value: employee,
+          done: false,
+        };
+      }
+      currentDepartmentIndex++;
+      currentEmployeeIndex = 0;
+      const nextDepartmentEmployees = departments[currentDepartmentIndex];
 
-                if (nextDepartmentEmployees) {
-                    return {
-                        value: nextDepartmentEmployees[currentEmployeeIndex],
-                        done: false,
-                    };
-                } else {
-                    return {
-                        done: true,
-                    };
-                }
-            }
-        },
-    };
+      if (nextDepartmentEmployees) {
+        return {
+          value: nextDepartmentEmployees[currentEmployeeIndex],
+          done: false,
+        };
+      }
+      return {
+        done: true,
+      };
+    },
+  };
 }

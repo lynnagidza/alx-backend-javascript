ed8b6eb495aa6797906eaf865b83caf4c9f421f0
Agidza <lynnagidza@gmail.com>
2023-06-29 00:00:31 +0300

    task 6 correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,5 +9,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    })))
+    .catch(() => []);
 }

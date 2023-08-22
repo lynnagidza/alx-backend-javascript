c9d1f751697aed31e78dcba7901f0cd30d3eeb37
Lynn Agidza <lynnagidza@gmail.com>
2023-08-22 21:18:16 +0300

    task 1 correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,20 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    console.log(`Your name is: ${name}`);
+    const trimmedName = name.toString().trim();
+    if (trimmedName.toLowerCase() === 'exit') {
+      console.log('This important software is now closing');
+      process.exit(0);
+    } else {
+      console.log(`Your name is: ${trimmedName}`);
+    }
   }
-}
-);
+});
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-}
-);
+});
 
 module.exports = process.stdin;
